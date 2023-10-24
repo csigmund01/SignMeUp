02de3d1b75eef1735c91b5950ec8d3d67fd4565b
csigmund01 <csigmund@purdue.edu>
2023-10-23 22:56:48 -0400

    Initial Commit

---
+++ SignMeUp/SignMeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignMeUpApp.swift
+//  SignMeUp
+//
+//  Created by Christopher Sigmund on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SignMeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
